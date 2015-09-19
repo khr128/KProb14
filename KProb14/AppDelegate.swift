bc584a6a68f5de58f683b4d7cfe52e627fca48f3
Dmitri Khrebtukov <khr128@gmail.com>
2015-09-19 12:04:32 -0700

    Initial Commit

---
+++ KProb14/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  KProb14
+//
+//  Created by khr on 9/19/15.
+//  Copyright © 2015 khr. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(aNotification: NSNotification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(aNotification: NSNotification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
---
+++ KProb14/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  KProb14
+//
+//  Created by khr on 9/19/15.
+//  Copyright © 2015 khr. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: AnyObject? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
